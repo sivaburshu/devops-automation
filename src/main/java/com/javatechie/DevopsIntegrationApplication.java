3fbc3d630547f7dfc34c16abfaa753843be09425
sivaburshu <sivaburshu@emmsonsinfotech.com>
2024-09-30 13:31:20 +0530

    Update DevopsIntegrationApplication.java

--- src/main/java/com/javatechie/DevopsIntegrationApplication.java
+++ src/main/java/com/javatechie/DevopsIntegrationApplication.java
@@ -11,7 +11,7 @@ public class DevopsIntegrationApplication {
 
 	@GetMapping
 	public String message(){
-		return "welcome to emmsons infotech my dear";
+		return "welcome to emmsons infotech";
 	}
 
 	public static void main(String[] args) {
